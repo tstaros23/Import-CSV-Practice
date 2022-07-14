2fa8c8f787bd1b492ff890620d42227706b2ec7d
Ted Staros <tstaros23@gmail.com>
2022-07-14 14:43:19 -0600

    Create file uploader that associates users to groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,13 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def import
+    Group.import(params[:file])
+    redirect_to groups_path, notice: "Groups Added Successfully"
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,13 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   validates_presence_of :name
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      attributes = row.to_hash
+      group = Group.find_or_create_by(name: attributes['Group Name'])
+      user = User.find_or_create_by(first_name: attributes['First Name'], last_name: attributes['Last Name'])
+      user_group = UserGroup.find_or_create_by(user_id: user.id, group_id: group.id, role: attributes['Role in Group'])
+    end
+  end
 end
